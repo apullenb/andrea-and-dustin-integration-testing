d9c0340df932ff387118dbb318ef81b98ec72cbc
Andrea Pullen <apullenb@gmail.com>
2020-10-21 13:48:03 -0500

    fix issues

--- app.js
+++ app.js
@@ -1,63 +1,56 @@
 const express = require('express');
 const morgan = require('morgan');
 
-const AppsData = require('./AppsData');
-
 const app = express();
+app.use(morgan('common'));
+const plays = require('./playstore.js')
 
-app.use(morgan('common')); 
+const playStore = require('./playstore');
 
 app.get('/apps', (req, res) => {
-// accepts query params of sort or genres
-// sort can be rating or app
-// genres can be Action, Puzzle, Strategy, Casual, Arcade, Card
-  const {sort, genres = ""} = req.query;
-  const genresArr = ['action', 'puzzle', 'strategy', 'casual', 'arcade', 'card'];
-  let results = AppsData;
+  const {sort, genre} = req.query;
+  let returnData = [...playStore];
 
-  if(!genres){
-      return res.status(400).send('Genre is required as search parameter')
+  if(genre && genre !== 'action' && genre !== 'puzzle' && genre !== 'strategy' && genre !== 'casual' && genre !== 'arcade' && genre !== 'card') {
+    res
+      .status(400)
+      .send('Genre must be one of the following : action, puzzle, strategy, casual, arcade, card');
   }
 
-  if(genres && !genresArr.includes(genres.toLowerCase())){
-    return res.status(400)
-      .send('Genre must be of type Action, Puzzle, Strategy, Casual, Arcade, or Card');
+  // sort validation
+  if(sort && sort !== 'rating' && sort !== 'app') {
+    res
+      .status(400)
+      .send('Sort must be one of rating or app');
   }
 
-  if(genres){
-    results = results.filter(app => app.Genres.toLowerCase().includes(genres.toLowerCase()));
-  }
-  
-  if(sort && !['rating', 'app'].includes(sort.toLowerCase())){
-      return res.status(400).send('Sort must be one of rating or app');
+  // genre filtering
+  if(genre) {
+    returnData = returnData.filter(a => {
+      return a.Genres.toLowerCase().includes(genre.toLowerCase());
+    });
   }
 
-  if(sort){
-    let temp = sort.toLowerCase();
-    temp = sort.charAt(0).toUpperCase() + temp.slice(1);
+
+  // sort by rating or app
+  if(sort) {
+    let newSort;
+
+    if(sort === 'rating'){
+      newSort = 'Rating';
+    } else {
+      newSort = 'App';
+    }
     
-    results.sort((a, b) => {
-      if(temp !== 'Rating'){
-        a = a[temp].toLowerCase();
-        b = b[temp].toLowerCase();
-      } else {
-        a = a[temp];
-        b = b[temp];
-      }
-      
-      if(a < b){
-        return -1;
-      } else if(a > b){
-        return 1;
-      } else {
-        return 0;
-      }
-    })
+    returnData.sort((currApp, nextApp) => {
+      console.log('currApp rating', currApp[newSort]);
+      if(currApp[newSort] > nextApp[newSort]) return 1;
+      if(currApp[newSort] < nextApp[newSort]) return -1;
+      return;
+    });
   }
 
-  res.json(results)
+  res.json(returnData);
 });
 
-app.listen(8000, () => {
-    console.log('Server started on PORT 8000');
-  });  
+module.exports = app
